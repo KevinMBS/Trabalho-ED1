0a0373b8a6392a22f9469a40c1717c1e1fcd95fb
Kevin Maykon Batalha Santos <you@example.com>
2020-09-19 00:22:11 -0300

    Criação de lista Encadeada Básica

---
+++ src/Lista/ListaEncadeada.java
@@ -0,0 +1,12 @@
+package Lista;
+
+import trabalhoed1.elementos.Arquivo;
+
+public class ListaEncadeada {
+    private Arquivo raiz;
+    
+    public ListaEncadeada(){
+        this.raiz = null;
+    }
+    
+}
